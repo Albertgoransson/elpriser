39a84b7a3851e7e3991f40a7288341bbb10b38e5
Albertgoransson <albert.goransson@outlook.com>
2026-06-11 12:24:04 +0200

    elpriser website created

---
+++ sw.js
@@ -0,0 +1,21 @@
+// En minimal Service Worker som krävs för att göra sidan till en PWA
+const CACHE_NAME = 'elpriser-v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                './elpriser.html',
+                './elomraden-karta.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
